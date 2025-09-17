14cb9692bef1d01fe4ffd2255ad85d7a56cb0735
Phantum-echo <thealphacentury224@gmail.com>
2025-09-17 21:50:28 +0530

    Props

--- FragmentProps/Props/src/App.jsx
+++ FragmentProps/Props/src/App.jsx
@@ -1,22 +1,26 @@
 function App() {
-  //let verse = [];
-  let verse = [
-    "Tsunade",
-    "Kyomii",
-    "Kuboshibo",
-    "Upper Three",
-    "Moon Breathing",
-  ];
+  let verse = [];
+  // let verse = [
+  //   "Tsunade",
+  //   "Kyomii",
+  //   "Kuboshibo",
+  //   "Upper Three",
+  //   "Moon Breathing",
+  // ];
 
   // if (verse.length === 0) {
   //   return <h2>Hey there is nothing to print</h2>;
   // }
 
+  // let emptymsg =
+  //   verse.length === 0 ? <h2>Hey there is nothing to print</h2> : null;
+
   return (
     <>
       <center>
         <h1>The Unique Universe</h1>
-        {verse.length === 0 ? <h2>Hey there is nothing to print</h2> : null}
+        {/* {emptymsg} */}
+        {verse.length === 0 && <h2>Hey there is nothing to print</h2>}
         <ul className="list-group">
           {verse.map((item) => (
             <li key={item} className="item-group-list">
